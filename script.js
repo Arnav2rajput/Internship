6fd2dbea497fc2a23da9063af0d5d955e13b850f
Arnav2rajput <arnav2rajput@gmail.com>
2024-12-22 20:54:43 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+    let slides = document.getElementsByClassName("mySlides");
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none"; // Hide all slides
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) { slideIndex = 1 } // Reset to first slide
+    slides[slideIndex - 1].style.display = "block"; // Show the current slide
+    setTimeout(showSlides, 3000); // Change slide every 3 seconds
+}
+
+function plusSlides(n) {
+    slideIndex += n;
+    let slides = document.getElementsByClassName("mySlides");
+    if (slideIndex > slides.length) { slideIndex = 1 }
+    if (slideIndex < 1) { slideIndex = slides.length }
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none"; // Hide all slides
+    }
+    slides[slideIndex - 1].style.display = "block"; // Show the current slide
+}
